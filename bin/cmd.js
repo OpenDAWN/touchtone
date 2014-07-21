b1bce49b57406ce50a992b6f5789f28b59536fde
James Halliday <mail@substack.net>
2014-07-21 13:02:44 -0700

    number mode works

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,16 +1,24 @@
 var baudio = require('baudio');
 var tone = require('../')();
+var minimist = require('minimist');
+var argv = minimist(process.argv.slice(2));
 
-process.stdin.setRawMode(true);
-process.stdin.on('data', function (buf) {
-    if (buf[0] === 3 || buf[0] === 4) {
-        process.stdin.setRawMode(false);
-        process.exit();
-    }
-    else {
-        for (var i = 0; i < buf.length; i++) {
-            tone.press(String.fromCharCode(buf[i]));
+var keys = argv._.join('').split('');
+keys.forEach(function (key) { tone.press(key) });
+
+if (argv.i) {
+    process.stdin.setRawMode(true);
+    process.stdin.on('data', function (buf) {
+        if (buf[0] === 3 || buf[0] === 4) {
+            process.stdin.setRawMode(false);
+            process.exit();
         }
-    }
-});
+        else {
+            for (var i = 0; i < buf.length; i++) {
+                tone.press(String.fromCharCode(buf[i]));
+            }
+        }
+    });
+}
+
 baudio(tone.play()).play();
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var defined = require('defined');
 var columns = [ 1209, 1336, 1477, 1633 ];
 var rows = [ 697, 770, 852, 941 ];
 var labels = '123a456b789c*0#d'.split('');
@@ -16,28 +17,34 @@ module.exports.tones = tones;
 function Touchtone (opts) {
     if (!(this instanceof Touchtone)) return new Touchtone(opts);
     if (!opts) opts = {};
-    this._duration = opts.duration || 0.5;
-    this._tone = null;
+    this._duration = opts.duration || 1/8;
+    this._pause = defined(opts.pause, this._duration / 4);
+    this._tones = [];
 }
 
 Touchtone.prototype.press = function (key) {
     var t = tones[key];
     if (!t) return;
-    this._tone = [ t, null ];
+    this._tones.push([ t, null ]);
+    if (this._pause) this._tones.push([ 'pause', null ]);
 };
 
 Touchtone.prototype.play = function () {
     var self = this;
     return function (t) {
-        var tone = self._tone;
+        var tone = self._tones[0];
         if (!tone) return 0;
         if (tone[1] === null) {
             tone[1] = t;
         }
-        if (tone[1] < t - self._duration) {
-            self._tone = null;
+        if (tone[0] === 'pause' && tone[1] < t - self._pause) {
+            self._tones.shift();
+        }
+        else if (tone[1] < t - self._duration) {
+            self._tones.shift();
             return 0;
         }
+        if (tone[0] === 'pause') return 0;
         return (sin(tone[0][0]) + sin(tone[0][1])) / 2;
         
         function sin (x) { return Math.sin(2 * Math.PI * x * t) }
